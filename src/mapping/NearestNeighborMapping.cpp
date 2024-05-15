72fda095df91da6177413997b4aea8cce5dfcf30
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2024-05-15 17:28:07 +0200

    Try using inverse distance weighting for NN

--- src/mapping/NearestNeighborMapping.cpp
+++ src/mapping/NearestNeighborMapping.cpp
@@ -4,12 +4,13 @@
 #include <boost/container/flat_set.hpp>
 #include <functional>
 #include "logging/LogMacros.hpp"
+#include "mapping/RadialBasisFctSolver.hpp"
+#include "mapping/impl/BasisFunctions.hpp"
 #include "profiling/Event.hpp"
 #include "profiling/EventUtils.hpp"
 #include "utils/EigenHelperFunctions.hpp"
 #include "utils/IntraComm.hpp"
 #include "utils/assertion.hpp"
-
 namespace precice::mapping {
 
 NearestNeighborMapping::NearestNeighborMapping(
@@ -36,9 +37,12 @@ void NearestNeighborMapping::mapConservative(const time::Sample &inData, Eigen::
   Eigen::VectorXd &      outputValues = outData;
 
   // Data dimensions (for scalar = 1, for vectors > 1)
-  const size_t inSize          = input()->nVertices();
-  const int    valueDimensions = inData.dataDims;
+  const size_t    inSize = input()->nVertices();
+  Eigen::VectorXd weights(output()->nVertices());
+  weights.setZero();
+  const int valueDimensions = inData.dataDims;
 
+  CompactPolynomialC0 basis_function(0.5);
   for (size_t i = 0; i < inSize; i++) {
     int const outputIndex = _vertexIndices[i] * valueDimensions;
 
@@ -47,9 +51,20 @@ void NearestNeighborMapping::mapConservative(const time::Sample &inData, Eigen::
       const int mapOutputIndex = outputIndex + dim;
       const int mapInputIndex  = (i * valueDimensions) + dim;
 
-      outputValues(mapOutputIndex) += inputValues(mapInputIndex);
+      const auto &v                 = input()->vertex(mapInputIndex).rawCoords();
+      auto        u                 = output()->vertex(mapOutputIndex).rawCoords();
+      double      squaredDifference = computeSquaredDifference(u, v);
+      auto        local_weight      = basis_function.evaluate(std::sqr(squaredDifference));
+      weights[mapOutputIndex] += local_weight;
+
+      outputValues(mapOutputIndex) += local_weight * inputValues(mapInputIndex);
     }
   }
+  for (size_t i = 0; i < output()->nVertices(); i++) {
+    if (weights[i] > 0)
+      outputValues[i] = outputValues[i] / weights[i];
+  }
+
   PRECICE_DEBUG("Mapped values = {}", utils::previewRange(3, outputValues));
 }
 
